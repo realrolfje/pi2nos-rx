3a09d94a7047b120c971c238794a9a8931532d89
Rolf <no-reply@example.com>
2015-01-05 23:11:07 +0100

    Updated exact locations of receivers, thanks to Mischa for the KML file with the info.

--- html/configuration.js
+++ html/configuration.js
@@ -13,52 +13,54 @@ var myOptions = {
   mapTypeControl: false
 }
 
+"Rx_Hilversum,Rx_Utrecht,Rx_Amsterdam,Rx_Zierikzee,Rx_Apeldoorn,Dailyminutes"
+
 var receivers = {
 	  "type": "FeatureCollection",
 	  "features": [
 		{
+		  "id":"Rx_Hilversum",
 		  "type": "Feature",
 		  "geometry": {
 			"type": "Point",
-			"coordinates": [5.1729, 52.2368]
+			"coordinates": [5.164326284803233,52.24249959861779]
 		  },
-		  "id":"Rx_Hilversum",
 		  "properties": {
 			"signal": "10",
 			"squelch" : "closed"
 		  }
 		},
 		{
-		  "type": "Feature",
-		  "geometry": {
-			"type": "Point",
-			"coordinates": [5.022297, 52.100284]
-		  },
 		  "id":"Rx_Utrecht",
+		  "type": "Feature",
+		  "geometry": {
+			"type": "Point",
+			"coordinates": [5.053434032571373, 52.00992614369851]
+		  },
 		  "properties": {
 			"signal": "10",
 			"squelch" : "closed"
 		  }
 		},
         {
-		  "type": "Feature",
-		  "geometry": {
-			"type": "Point",
-			"coordinates": [3.924334, 51.653101]
-		  },
 		  "id":"Rx_Zierikzee",
+		  "type": "Feature",
+		  "geometry": {
+			"type": "Point",
+			"coordinates": [3.931893379717417,51.64707776608346]
+		  },
 		  "properties": {
 			"signal": "10",
 			"squelch" : "closed"
 		  }
 		},
         {
-		  "type": "Feature",
-		  "geometry": {
-			"type": "Point",
-			"coordinates": [4.929472, 52.408901]
-		  },
 		  "id":"Rx_Amsterdam",
+		  "type": "Feature",
+		  "geometry": {
+			"type": "Point",
+			"coordinates": [4.78980338760532,52.36033042450219]
+		  },
 		  "properties": {
 			"signal": "10",
 			"squelch" : "closed"
@@ -66,16 +68,29 @@ var receivers = {
 		},
         {
 		  "type": "Feature",
+		  "id":"Rx_Apeldoorn",
 		  "geometry": {
 			"type": "Point",
-			"coordinates": [6.022805, 52.239973]
+			"coordinates": [5.90613085257128,52.22481977473116]
 		  },
-		  "id":"Rx_Apeldoorn",
 		  "properties": {
 			"signal": "10",
 			"squelch" : "closed"
 		  }
 		}
+		/*  Fake repeater used for John's "Daily Minutes" transmissions
+		,{
+		  "type": "Feature",
+		  "geometry": {
+			"type": "Point",
+			"coordinates": [5.247611, 52.564620]
+		  },
+		  "id":"Dailyminutes",
+		  "properties": {
+			"signal": "10",
+			"squelch" : "closed"
+		  }
+		} */
 	  ]
 	}
 	
